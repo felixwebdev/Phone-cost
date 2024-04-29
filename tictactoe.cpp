074b5e7a4901f8c2faccc5816377e188bb6bc0d6
Tran Khanh Coder <91669973+felixwebdev@users.noreply.github.com>
2024-04-29 11:02:46 +0700

    Update tictactoe.cpp finallllllllllllllllllllll

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -6,16 +6,14 @@
 #include <stdexcept>
 using namespace std;
 
-// Xác đinh hằng số X và O với kí tự X - Màu vàng và O - Màu xanh ( ANSI escape code)
 #define X "\033[33mX\033[0m"
 #define O "\033[32mO\033[0m"
 
-// Khai báo các màu nền tương ứng sử dụng ANSI escape code
 string COLOR_RESET = "\033[0m";
 string COLOR_BACKGROUND_GREEN = "\033[42m";
 string COLOR_BACKGROUND_RED = "\033[41m";
 string COLOR_BACKGROUND_BLUE = "\033[44m";
-// In ra dải màu nền
+
 void print(string& colorCode, int width) {
     cout << colorCode;
     for (int i = 0; i < width; ++i) {
@@ -24,7 +22,6 @@ void print(string& colorCode, int width) {
     cout << COLOR_RESET;
 }
 
-// ẨN con trỏ trên màn hình console
 void hideCursor(bool check = FALSE)
 {
     CONSOLE_CURSOR_INFO Info;
@@ -33,7 +30,6 @@ void hideCursor(bool check = FALSE)
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Info);
 }
 
-// Đặt màu cho các kí tự xuất hiện trên màn hình console
 void SET_COLOR(int color)
 {
     WORD wColor;
@@ -46,21 +42,17 @@ void SET_COLOR(int color)
     }
 }
 
-// Lớp cơ sở Board của game
 class Board {
 private:
     vector<string> board;
 
 public:
-    // Hàm khởi tạo 
     Board() {
         board = vector<string>(9) = {"1","2","3","4","5","6","7","8","9"};
     }
 
-    // Hàm hủy
     ~Board() {} 
 
-    // Hàm in bảng
     void printBoard() const {
         print(COLOR_BACKGROUND_GREEN, 15);
         cout << endl;
@@ -80,17 +72,14 @@ public:
         }
     }
 
-    // Hàm kiểm tra ô còn trống hay không
     bool isEmptyCell(const string s) const {
         return (s >= "1" && s <= "9");
     }
 
-    // Hàm lấy giá trị từ một ô trong bảng
     string getCell(int i) const {
         return board[i];
     }
 
-    // Hàm kiểm tra bảng đã hết ô trống hay chưa 
     bool isFull() const {
         for (string cell : board) {
             if (isEmptyCell(cell)) return false;
@@ -98,7 +87,6 @@ public:
         return true;
     }
 
-    // Hàm điền kí tự vào bảng và xử lí ngoại lệ 
     void placeMark(int pos, string mark) {
         if (pos < 0 || pos >= 9) {
             SET_COLOR(4);
@@ -112,94 +100,73 @@ public:
         board[pos] = mark;
     }
 
-    //Hàm kiểm tra và trả về giá trị là dấu X hoặc O tuơng ứng với người chiến thắng
     string Winner() const {
-        // Kiểm tra hàng
         for (int i = 0; i < 9; i += 3) {
             if (!isEmptyCell(board[i]) && board[i] == board[i + 1] && board[i] == board[i + 2]) return board[i];
         }
-        // Kiểm tra cột 
+
         for (int i = 0; i < 3; ++i) {
             if (!isEmptyCell(board[i]) && board[i] == board[i + 3] && board[i] == board[i + 6]) return board[i];
         }
-        // Kiểm tra đường chéo 
+
         if (!isEmptyCell(board[0]) && board[0] == board[4] && board[0] == board[8]) return board[0];
         if (!isEmptyCell(board[2]) && board[2] == board[4] && board[2] == board[6]) return board[2];
 
-        // Nếu chưa có người chiến thắng thì trả về dấu khoảng trắng
         return " ";
     }
 };
 
-// Lớp cơ sở Player 
+
 class Player {
 private:
     string mark;
 public:
-    // Hàm khởi tạo
     Player(string _mark = " ") { mark = _mark; }
     Player(const Player& p)    { mark = p.mark; }
-
-    // Hàm hủy 
     ~Player() {}
 
-    // Hàm thuần ảo Move để trả về vị trí mà người chơi muốn đi
     virtual int Move(const Board& board) = 0;
-    // Hàm thuần ảo Identity trả về danh tính người chơi
     virtual string Identity() = 0;
 };
 
-// Lớp Human thể hiện đối tượng là con người kế thừa từ lớp cơ sở Player 
 class Human : public Player {
 public:
-    // Hàm khởi tạo
     Human(string m = " ") : Player(m) {}
 
-    // Hàm hủy
     ~Human() {}
-    // Định nghĩa lại hàm thuần ảo kế thừa từ lớp cơ sở
+
     int Move(const Board& board) {
         int pos;
         cout << "Enter your move (1-9): ";
         cin >> pos;
         return pos - 1;
     }
-    // Định nghĩa lại hàm thuần ảo kế thừa từ lớp cơ sở
     string Identity() { return "You"; }
 };
 
-// lớp Computer thể hiện người chơi là máy tính kế thừa từ lớp Player
 class Computer : public Player {
 private:
     int level;
     string playWith;
 public:
-    // Hàm khởi tạo
     Computer(string m = " ", int _level = 1, string _playWith = "Human") : Player(m) {
         level = _level;
         playWith = _playWith;
     }
 
-    // Hàm hủy
     ~Computer() {}
 
-    // Phương thức getter - setter
     void setLevel(int _level) { level = _level;}
     int getLevel() { return level;         }
     void setPlayWith(string _playWith) { playWith = _playWith; }
     string setPlayWith(){ return playWith;      }
 
-    // Định nghĩa lại hàm kế thừa từ lớp Player
     string Identity() { return "Computer"; }
 
     bool Run = true;
-    // Định nghĩa lại hàm kế thừa từ lớp cơ sở
     int Move(const Board& board) {
-        // Nếu đối thủ là máy tính thì đặt độ trễ là 1 giây để người dùng dễ quan sát trận đấu
         if (playWith == "Computer") Sleep(1000);
-        // Độ khó cao nhất
         if (level == 3) {
-            // Chặn giữa đường chéo
             if (Run) {
                 Run = false;
                 if (board.isEmptyCell(board.getCell(4))) return 4;
@@ -208,20 +175,16 @@ public:
                 if (board.isEmptyCell(board.getCell(6))) return 6;
                 if (board.isEmptyCell(board.getCell(8))) return 8;
             }
-            // Chặn giữa hàng 
             for (int i = 0; i < 9; i += 3) {
                 if (!board.isEmptyCell(board.getCell(i)) && board.getCell(i) == board.getCell(i + 2) && board.isEmptyCell(board.getCell(i+1))) return i + 1;
             }
-            // Chặn giữa cột
             for (int i = 0; i < 3; ++i) {
                 if (!board.isEmptyCell(board.getCell(i)) && board.getCell(i) == board.getCell(i + 6) && board.isEmptyCell(board.getCell(i+3))) return i + 3;
             }
         }
 
-        // Độ khó bình thường
         if (level >= 2) {
 
-            // Chặn hàng
             for (int i = 0; i < 9; i += 3) {
                 if (!board.isEmptyCell(board.getCell(i)) && board.getCell(i) == board.getCell(i + 1) && board.isEmptyCell(board.getCell(i+2))) return i + 2;
             }
@@ -229,7 +192,6 @@ public:
                 if (!board.isEmptyCell(board.getCell(i)) && board.getCell(i) == board.getCell(i - 1) && board.isEmptyCell(board.getCell(i-2))) return i - 2;
             }
 
-            // Chặn cột
             for (int i = 0; i < 3; ++i) {
                 if (!board.isEmptyCell(board.getCell(i)) && board.getCell(i) == board.getCell(i + 3) && board.isEmptyCell(board.getCell(i+6))) return i + 6;
             }
@@ -237,14 +199,12 @@ public:
                 if (!board.isEmptyCell(board.getCell(i)) && board.getCell(i) == board.getCell(i - 3) && board.isEmptyCell(board.getCell(i-6))) return i - 6;
             }
 
-            // Chặn đường chéo
             if (!board.isEmptyCell(board.getCell(0)) && board.getCell(0) == board.getCell(4) && board.isEmptyCell(board.getCell(8))) return 8;
             if (!board.isEmptyCell(board.getCell(8)) && board.getCell(8) == board.getCell(4) && board.isEmptyCell(board.getCell(0))) return 0;
             if (!board.isEmptyCell(board.getCell(2)) && board.getCell(2) == board.getCell(4) && board.isEmptyCell(board.getCell(6))) return 6;
             if (!board.isEmptyCell(board.getCell(6)) && board.getCell(6) == board.getCell(4) && board.isEmptyCell(board.getCell(2))) return 2;
         }
 
-        // Cấp độ dễ nhất - Các nước đi đều là ngẫu nhiên
         int tmp = rand() % 9;
         while (tmp < 0 || tmp >= 9 || !board.isEmptyCell(board.getCell(tmp))) {
             tmp = rand() % 9;
@@ -253,24 +213,20 @@ public:
     }
 };
 
-// Lớp cơ sở Game
 class Game {
 private:
-    Board board;       // đại diện cho bảng trò chơi
-    Player* player1;   // đại diện cho người chơi thứ nhất 
-    Player* player2;   // đại diện cho người chơi thứ hai 
+    Board board;      
+    Player* player1;   
+    Player* player2;   
 
 public:
-    // Hàm khởi tạo
     Game(Player* p1 = NULL, Player* p2 = NULL) {
         player1 = p1;
         player2 = p2;
     }
 
-    // Hàm hủy
     ~Game() {}
 
-    // In Menu chính
     void mainMenu() {
         SetConsoleTitle(L"Tic Tac Toe");
         cout << "+-----------------------------------------------+\n";
@@ -286,7 +242,6 @@ public:
         cout << "Enter your choice: ";
     }
 
-    // In Menu phụ
     void subMenu() {
         system("cls");
         SetConsoleTitle(L"Tic Tac Toe");
@@ -304,7 +259,6 @@ public:
         cout << "Enter your choice: ";
     }
 
-    // Hàm thực thi trò chơi
     void play() {
         Player* currentPlayer = player1;
         string currentMark = X;
@@ -323,9 +277,7 @@ public:
                 continue;
             }
 
-            // Thực hiện kiểm tra và in ra kết quả của trò chơi
             string winner = board.Winner();
-            // Chiến thắng 
             if (winner != " ") {
                 system("cls");
                 SET_COLOR(2);
@@ -345,7 +297,6 @@ public:
                 cout << "Press any key to continue.....";
                 break;
             }
-            // Hòa
             else if (board.isFull()) {
                 system("cls");
                 board.printBoard();
@@ -361,10 +312,8 @@ public:
         }
     }
 
-    // Hàm bắt đầu trò chơi
     void startGame() {
         while (true) {
-            // In ra menu chính để chọn chế độ chơi
             system("cls");
             mainMenu();
             int choice;
@@ -373,25 +322,20 @@ public:
             Player* player2 = NULL;
 
             cin >> choice;
-            // Người với máy tính
             if (choice == 1) {
-                // In ra menu phụ và cho phép ngừời chơi chọn cấp độ
                 subMenu();
                 cin >> level;
                 player1 = new Human(X);
                 player2 = new Computer(O, level);
                 system("cls");
             }
-            // Hai người
             else if (choice == 2) {
                 system("cls");
                 SetConsoleTitle(L"Tic Tac Toe");
                 player1 = new Human(X);
                 player2 = new Human(O);
             }
-            // Máy tính với máy tính
             else if (choice == 3) {
-                // Thực hiện set cấp độ ngẫu nhiên cho hai máy
                 srand(time(NULL));
                 player1 = new Computer(O, rand() % 3 + 1, "Computer");
                 player2 = new Computer(O, rand() % 3 + 1, "Computer");
